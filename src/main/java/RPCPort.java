24e4477de4505bed346e56293fc5abf072151a25
Hephlin <AbagaleHephlin@gmail.com>
2025-07-03 09:13:39 -0500

    Update RPCPort.java

--- src/main/java/RPCPort.java
+++ src/main/java/RPCPort.java
@@ -2,10 +2,74 @@ import java.net.Socket;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import sun.misc.Unsafe; // memory 0000L
+import java.lang.reflect.Field; // field 0000L
 
-import java.io.BufferInputStream;
-import java.io.BufferOutputStream;
 
-import java.lang.Integer;
-import jav.lang.Boolean;
+public class RPCPort extends List<BufferInputStream, LinkedList<BufferInputStream> {
 
+  /** 
+    * 
+    * to load buffer input stream with a long integer I would
+    * have to some how make java think the var is an address and so java
+    * will load it as too recalled types to my understanding.
+    *
+  */
+  static {
+    Object __address = (Long) address; 
+    while (__address != null && __address == (Long) address) {
+      __address = null;
+    }
+    try {
+    address != null;
+    BufferedInputStream buffer = new BufferedInputStream(
+      (FileInputStream)__address.getFileInputStream());
+      rRPCLoader0x0000L(buffer.read((byte)0x0000L), (Object)__address);
+    } catch (Exception except) {
+      except.getSuppressed();
+    }
+  }
+
+  /** 
+    *
+    * The function name was added with a 0x0000L so I expect as A
+    * kind of icon the memory address is a long integer byte address
+    * as a kind of core memory address look idk what ever
+    * and I did the same thing as shown up in the top
+    * I hopped data from one variable to another? switched*
+    *
+  */
+  private long address;
+  public static void rRPCLoader0x0000L(List <BufferedInputStream> buffer, Object object) {                         
+        Object rRecursiveObject[];
+        throws Exception {
+        Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafe.setAccessible(true);
+        Unsafe unsafe = (Unsafe) theUnsafe.get(null);
+        rRecursiveObject = null;
+        try {
+          while (address == rRecursiveObject 
+                 && rRecursiveObject == System.getenv("sun.misc.Unsafe")) {
+            rRecursiveObject = new FileInputStream("win64-x86" + File.seperator + 
+                                                   "rpc.dll");
+            ((File)rRecursiveObject).getAbsoluteFile();
+            buffer = new BufferedInputStream((File)rRecursiveObject).getFileInputStream());
+            for (((BufferedInputStream) buffer).read((byte)30000) : object) {
+              LinkedList<BufferInputStream> bufferObj;
+              bufferObj = buffer;
+              object = buffer;
+            };
+          }
+        } catch (Exception except) {
+          except.getSuppressed();
+        }
+        address = rRecursiveObject[]; 
+        
+        /** unsafe.allocateMemory(8); */
+        unsafe.putLong(address, 0x0000L);
+        unsafe.getLong(address);
+        unsafe.freeMemory(address);
+    }
+}
